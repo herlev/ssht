2fb17f4e668c8bb03581a1c7c60122055ed0d807
herlev <>
2024-09-03 18:38:45 +0200

    add command handling over a unix socket

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,69 @@
+use std::fs;
 use std::path::Path;
 use std::path::PathBuf;
 use std::str::FromStr;
 use tokio::io::Interest;
 use tokio::net::{UnixListener, UnixStream};
 
+enum Command {
+  // TODO: maybe refactor these into a single command that returns true if it moved and false otherwise
+  HasPane(Direction),
+  MovePane(Direction),
+}
+
+#[derive(Copy, Clone, Debug)]
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right,
+}
+
+impl Direction {
+  fn parse(s: &str) -> Option<Self> {
+    match s {
+      "up" => Some(Self::Up),
+      "down" => Some(Self::Down),
+      "left" => Some(Self::Left),
+      "right" => Some(Self::Right),
+      _ => None,
+    }
+  }
+}
+
+impl Command {
+  fn parse(s: &str) -> Option<Self> {
+    match s.split(' ').collect::<Vec<_>>()[..] {
+      ["has_pane", dir] => Some(Command::HasPane(Direction::parse(dir)?)),
+      ["move_pane", dir] => Some(Command::MovePane(Direction::parse(dir)?)),
+      _ => None,
+    }
+  }
+}
+
 async fn handle_client(ssh_session: &Session, stream: UnixStream) {
   let mut buffer = [0u8; 1024];
   stream.ready(Interest::READABLE | Interest::WRITABLE).await.unwrap();
-  stream.try_read(&mut buffer).unwrap();
-  let pane_above = pane_in_direction(ssh_session, Direction::Up).await;
-  stream
-    .try_write(format!("pane above? {}\n", if pane_above { "yes" } else { "no" }).as_bytes())
-    .unwrap();
+  let n = stream.try_read(&mut buffer).unwrap();
+
+  let s = String::from_utf8(buffer[0..n].to_vec()).unwrap();
+  let cmd = Command::parse(&s);
+
+  let r = match cmd {
+    Some(Command::HasPane(dir)) => (if pane_in_direction(ssh_session, dir).await {
+      "true"
+    } else {
+      "false"
+    })
+    .into(),
+    Some(Command::MovePane(dir)) => {
+      move_in_direction(ssh_session, dir).await;
+      "ok".into()
+    }
+    None => format!("unrecognized command {}", s),
+  };
+
+  stream.try_write(r.as_bytes()).unwrap();
 }
 
 async fn ipc_listener(ssh_session: &Session, socket_path: &Path) {
@@ -21,14 +73,6 @@ async fn ipc_listener(ssh_session: &Session, socket_path: &Path) {
   loop {
     let (stream, _addr) = listener.accept().await.unwrap();
     handle_client(ssh_session, stream).await
-    // match listener.accept().await {
-    //   Ok((stream, _addr)) => {
-    //     handle_client(session, stream).await;
-    //   }
-    //   Err(err) => {
-    //     eprintln!("Connection failed: {}", err);
-    //   }
-    // }
   }
 }
 
@@ -38,16 +82,23 @@ async fn run_tmux(control_path: &Path) {
   // the openssh library doesn't do pseudo-terminal allocation:
   // https://github.com/openssh-rust/openssh/issues/87
   // So just use ssh command manually instead
+
+  // From openssh rust library:
+  // ssh does not care about the addr as long as we have passed
+  // `-S &*self.ctl`.
+  // It is tested on OpenSSH 8.2p1, 8.9p1, 9.0p1
   let mut child = tokio::process::Command::new("ssh")
-    .arg("-t")
-    .arg("-S")
-    .arg(control_path)
-    // From openssh rust library:
-    // ssh does not care about the addr as long as we have passed
-    // `-S &*self.ctl`.
-    // It is tested on OpenSSH 8.2p1, 8.9p1, 9.0p1
-    .arg("none")
-    .arg("tmux")
+    .args([
+      "-t",
+      "-S",
+      control_path.to_str().unwrap(),
+      "none",
+      "tmux",
+      "new-session",
+      "-A",
+      "-s",
+      "main",
+    ])
     .stdin(Stdio::inherit())
     .stdout(Stdio::inherit())
     .stderr(Stdio::inherit())
@@ -56,12 +107,15 @@ async fn run_tmux(control_path: &Path) {
   let _ = child.wait().await.unwrap();
 }
 
-#[derive(Copy, Clone, Debug)]
-enum Direction {
-  Up,
-  Down,
-  Left,
-  Right,
+async fn move_in_direction(session: &Session, direction: Direction) {
+  let dir_str = format!("{:?}", direction).to_lowercase();
+  let tmux_dir = format!("-{}", dir_str.chars().next().unwrap().to_uppercase()); // -U -D -L -R
+  session
+    .command("tmux")
+    .args(["select-pane", &tmux_dir])
+    .status()
+    .await
+    .unwrap();
 }
 
 async fn pane_in_direction(session: &Session, direction: Direction) -> bool {
